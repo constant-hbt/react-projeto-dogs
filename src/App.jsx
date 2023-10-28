6bd221c99cf1b1175e9a2d73d974a0569eb85776
Henrique BT <henriqueborgesvotu@gmail.com>
2023-10-28 15:47:56 -0300

    :sparkles: feat: Criação do Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
 import './App.css';
-import Api from './api/Api';
 
 const App = () => {
   return (
     <div>
-      <Api />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 };
---
+++ src/Components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Dogs } from '../assets/dogs.svg';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link to="/" aria-label="Dogs - Home" className={styles.logo}>
+          <Dogs />
+        </Link>
+        <Link to="/login" className={styles.login}>
+          Login / Criar
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
