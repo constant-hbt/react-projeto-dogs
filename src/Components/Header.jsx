ca3c30ff57299d11f3c0819b591265bd178ed155
Henrique BT <henriqueborgesvotu@gmail.com>
2023-10-29 16:18:28 -0300

    :sparkles: feat: Adicionando o contexto de usuário

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,17 +2,26 @@ import React from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Dogs } from '../assets/dogs.svg';
+import { UserContext } from '../UserContext';
 
 const Header = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
         <Link to="/" aria-label="Dogs - Home" className={styles.logo}>
           <Dogs />
         </Link>
-        <Link to="/login" className={styles.login}>
-          Login / Criar
-        </Link>
+        {data ? (
+          <Link to="/conta" className={styles.login}>
+            {data.nome}
+          </Link>
+        ) : (
+          <Link to="/login" className={styles.login}>
+            Login / Criar
+          </Link>
+        )}
       </nav>
     </header>
   );
