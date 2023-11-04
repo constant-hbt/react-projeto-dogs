8976e3dfe3d56204193b47a0d09389004ff152d6
Henrique BT <henriqueborgesvotu@gmail.com>
2023-11-04 16:30:04 -0300

    :sparkles: feat: Adicionando modal de foto

---
+++ src/Components/Photo/PhotoComments.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { UserContext } from '../../UserContext';
+import PhotoCommentsForm from './PhotoCommentsForm';
+import styles from './PhotoComments.module.css';
+
+const PhotoComments = (props) => {
+  const [comments, setComments] = React.useState(() => props.comments);
+  const { login } = React.useContext(UserContext);
+  const commentsSection = React.useRef(null);
+
+  React.useEffect(() => {
+    commentsSection.current.scrollTop = commentsSection.current.scrollHeight;
+  }, [comments]);
+
+  return (
+    <>
+      <ul className={styles.comments} ref={commentsSection}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>
+              {comment.comment_author}: <span>{comment.comment_content}</span>
+            </b>
+          </li>
+        ))}
+      </ul>
+      {login && (
+        <PhotoCommentsForm
+          key={props.id}
+          id={props.id}
+          setComments={setComments}
+        />
+      )}
+    </>
+  );
+};
+
+export default PhotoComments;
---
+++ src/Components/Photo/PhotoCommentsForm.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styles from './PhotoCommentsForm.module.css';
+import { ReactComponent as Enviar } from '../../assets/enviar.svg';
+import useFetch from '../../Hooks/useFetch';
+import { COMMENT_POST } from '../../api';
+import Error from '../Helper/Error';
+
+const PhotoCommentsForm = ({ id, setComments }) => {
+  const [comment, setComment] = React.useState('');
+  const { request, error } = useFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const token = window.localStorage.getItem('token');
+    const { url, options } = COMMENT_POST(id, token, { comment });
+    const { response, json } = await request(url, options);
+    if (response.ok) {
+      setComment('');
+      setComments((comments) => [...comments, json]);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <textarea
+        value={comment}
+        onChange={({ target }) => setComment(target.value)}
+        name="comment"
+        id="comment"
+        placeholder="Comente..."
+        cols="30"
+        rows="10"
+        className={styles.textarea}
+      ></textarea>
+      <button className={styles.button}>
+        <Enviar />
+      </button>
+      <Error error={error} />
+    </form>
+  );
+};
+
+export default PhotoCommentsForm;
