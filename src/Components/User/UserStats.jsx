a33177cfd08c9ef5cd2d2f648597ccb10aad7e73
Henrique BT <henriqueborgesvotu@gmail.com>
2023-11-05 15:49:10 -0300

    :sparkles: feat: Adicionando gráficos com a biblioteca externa Victory e carregamento com lazy e suspense

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
 import Head from '../Helper/Head';
+import useFetch from '../../Hooks/useFetch';
+import { STATS_GET } from '../../api';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error';
+const UserStatsGraphs = React.lazy(() => import('./UserStatsGraphs'));
 
 const UserStats = () => {
-  <Head
-    title="Estatísticas"
-    description="Estatísticas de usuário do site Dogs"
-  />;
-  return <div>UserStats</div>;
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const token = window.localStorage.getItem('token');
+      const { url, options } = STATS_GET(token);
+      const { response, json } = await request(url, options);
+    }
+
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <React.Suspense fallback={<Loading />}>
+        <div>
+          <Head
+            title="Estatísticas"
+            description="Estatísticas de usuário do site Dogs"
+          />
+          <UserStatsGraphs data={data} />
+        </div>
+      </React.Suspense>
+    );
+
+  return null;
 };
 
 export default UserStats;
